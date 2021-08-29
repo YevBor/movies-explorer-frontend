a17d93bf062f8a4f4ef181e5a7bd4d1cec1ed985
Yevgeniy Borisovsky <yev.bor@yandex.ru>
2021-08-29 21:12:12 +0300

    small changes in Main area

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -2,12 +2,13 @@ import './Promo.css';
 import React from 'react';
 
 
-function Promo() {
+function Promo({children}) {
   
   return (
     <section className="promo">
       <div className="promo__container">
         <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        {children} 
       </div>
     </section>
   );
